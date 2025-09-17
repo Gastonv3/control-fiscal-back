6bc996f7c15c9628afb855b2ca874e94b1c6f8b9
Gastonv3 <gaston.92.08@gmail.com>
2025-09-17 11:45:14 -0300

    Se aumenta el limte de request por ip a 500 cada 15 minutos

--- src/middlewares/rate-limit.middleware.ts
+++ src/middlewares/rate-limit.middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 20, // límite de 5 requests por IP en ese tiempo
+  max: 500, // límite de 400 requests por IP en ese tiempo
   message: {
     status: 429,
     message: "Demasiadas solicitudes desde esta IP, intentá más tarde.",
